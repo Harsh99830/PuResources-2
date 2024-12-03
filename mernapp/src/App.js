5ee383286e307060581b65c389eec3fdb8dbb99e
harsh agrawal <harshagrawal7878@gmail.com>
2024-12-03 12:01:47 +0530

    Backend deployed

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -13,7 +13,7 @@ export default function App() {
 
   const loadData = async () => {
     setLoading(true); // Set loading to true when fetching data
-    let response = await fetch("https://pu-resourcess.vercel.app/api/Data", {
+    let response = await fetch("https://pu-resources-api.vercel.app/api/Data", {
       method: "POST",
       headers: {
         "content-type": "application/json"
