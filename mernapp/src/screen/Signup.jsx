c3a72d64c2026d8de5e09cb4db1e5a30a1dae35c
harsh agrawal <harshagrawal7878@gmail.com>
2024-12-03 23:09:50 +0530

    changed

--- mernapp/src/screen/Signup.jsx
+++ mernapp/src/screen/Signup.jsx
@@ -14,7 +14,7 @@ export default function Signup() {
         event.preventDefault();
         setLoading(true);  // Start loading
 
-        const response = await fetch("https://poornima-resources.vercel.app/api/createuser", {
+        const response = await fetch("https://pu-resources-api.vercel.app//api/createuser", {
             mode: 'no-cors',
             method: 'POST',
             headers: {
